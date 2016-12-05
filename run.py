e861a83149da8f90d7b10404993da0b2b0a0789d
Maciek Kasprzyk <mkasprzyk@szy.fr>
2016-12-05 13:54:17 +0100

    Add flask restful to handle graph updates

--- run.py
+++ run.py
@@ -1,22 +1,30 @@
 from flask import Flask, jsonify, render_template, request
-from redis import Redis
+from flask_restful import Resource, Api
 from utils import make_celery
 
 app = Flask(__name__)
+api = Api(app)
 app.config.update(
     CELERY_BROKER_URL='redis://localhost:6379',
     CELERY_RESULT_BACKEND='redis://localhost:6379'
 )
 celery = make_celery(app)
 
-@celery.task
-def update_status():
-    return True
 
 @app.route('/')
 def index():
     result = update_status.delay()
     return render_template('index.html')
+
+@celery.task
+def update_status():
+    return True
+
+class Pipeline(Resource):
+    def get(self):
+        return {}
+
+api.add_resource(Pipeline, '/pipeline')
 
 if __name__ == '__main__':
     app.run()
