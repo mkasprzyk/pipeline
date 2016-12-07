d153483fa41e7803c000abb4140e599363dfd008
Maciek Kasprzyk <mkasprzyk@szy.fr>
2016-12-07 11:00:58 +0100

    Remove auto updates. Events should be triggered by backend

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-export JENKINS_URL=http://localhost:8080
-export JENKINS_USERNAME=
-export JENKINS_PASSWORD=
+export JENKINS_URL=http://192.168.59.103:8080
+export JENKINS_USERNAME=mkasprzyk
+export JENKINS_PASSWORD=mietek05
 
 PYTHON=$(which python)
 
